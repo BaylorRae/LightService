2dc5641a1b1d6089b4a5e109a4622b79875d31a0
Baylor Rae' <baylorrae@gmail.com>
2013-09-17 21:43:31 -0500

    add Organizer and Context classes

---
+++ LightService/Organizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LightService {
+    public class Organizer {
+        public Context context = null;
+
+        public Organizer() {
+            this.context = new Context();
+        }
+
+        public static Organizer With() {
+            return new Organizer();
+        }
+
+    }
+}
---
+++ LightServiceTest/OrganizerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LightServiceTest {
+    [TestClass]
+    public class OrganizerTest {
+
+        [TestMethod]
+        public void WithCreatesADefaultContext() {
+            Assert.IsTrue(LightService.Organizer.With().context is LightService.Context, "Organizer default context wasn't an instance of LightService.Context");
+        }
+
+    }
+
+    //internal class ExampleOrganizer : LightService.Organizer {
+
+    //    public static bool SomeMethod() {
+    //        return true;
+    //    }
+
+    //    public static bool Add1and1() {
+    //        LightService.Action[] actions = new LightService.Action[] {
+    //            new ExampleAction()
+    //        };
+
+    //        return With(new { number = 1 }).Reduce(actions);
+    //    }
+
+    //}
+
+    //internal class ExampleAction : LightService.Action {
+
+    //    public LightService.Context executed(LightService.Context context) {
+    //        context.result = context.number + context.number;
+    //        return context;
+    //    }
+    //}
+}
